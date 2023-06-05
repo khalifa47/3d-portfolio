42a3808e4a62df0105771034a7915e2a41a7e966
khalifa47 <khalifafumo5@gmail.com>
2023-06-05 10:12:15 +0300

    tech stack set up

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import { BallCanvas } from "./canvas";
+import { SectionWrapper } from "../hoc";
+import { technologies } from "../constants";
 
 const Tech = () => {
   return (
-    <div>Tech</div>
-  )
-}
+    <div className="flex flex-row flex-wrap justify-center gap-10">
+      {technologies.map((technology) => (
+        <div className="relative w-28 h-28" key={technology.name}>
+          <BallCanvas icon={technology.icon} />
+          <div className="text-center text-sm text-slate-300 ">
+            {technology.name}
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
 
-export default Tech
+export default SectionWrapper(Tech, "");
